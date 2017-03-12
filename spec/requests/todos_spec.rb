4aec776ae6525df711c157b734f916ffa9a6807a
gojicmiroslav <miroslav.gojic78@gmail.com>
2017-03-12 22:52:23 +0100

    Eager loading in Todos request spec

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Todos API', type: :request do
 
-	let(:todos) { create_list(:todo, 10) }
+	let!(:todos) { create_list(:todo, 10) }
 	let(:todo_id) { todos.first.id }
 
 	# Test suite for GET /todos
